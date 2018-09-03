19e9d4e7ad0388484b03f6acac237ff150088788
stan correy <stancorrey@MacBook-Pro-57.local>
2018-09-03 20:37:22 +1000

    Identify "" as not being a palindrome

--- index.js
+++ index.js
@@ -5,23 +5,27 @@ String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
 }
 
-// Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
 
-  // Returns content processed for palindrome testing.
-    this.processedContent = function processedContent() {
-      return this.letters().toLowerCase();
-    }
+  // Return content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.letters().toLowerCase();
+  }
 
-      // Returns the letters in the content.
-    this.letters = function letters() {
-      return  Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
-
-      }
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
+  }
 
   // Returns true if the phrase is a palindrome, false otherwise.
-    this.palindrome = function palindrome() {
+  this.palindrome = function palindrome() {
+    if (this.letters()) {
       return this.processedContent() === this.processedContent().reverse();
+    } else {
+      return false;
+    }
   }
- }
+}
